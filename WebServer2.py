1a560cbcb19d43854c64bd46da26bced8c85ef03
Sander van der Maar <sandervandermaar@gmail.com>
2021-11-13 10:03:06 -0300

    Implements simple http/tcp server.

--- WebServer2.py
+++ WebServer2.py
@@ -1,1 +1,12 @@
+# About localhost:
+#    https://pt.wikipedia.org/wiki/Localhost
 
+import http.server
+import socketserver
+
+PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Serving at port: " + str(PORT))
+    httpd.serve_forever();
